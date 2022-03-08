fdb9798f2f2579abe285025c683b324d5e9c9ffe
Mehdi Tagaizine <mtagaizi@namek.io>
2022-03-08 20:25:05 +0100

    feat: added node nft block

---
+++ components/NodeNft.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="node-nft">
+    <div class="node-nft__title">{{ name }}</div>
+    <div class="node-nft__blue-text my-1">
+      Earn {{ dailyEarnings }} $POLAR / day
+    </div>
+    <div class="divider mt-2" />
+
+    <div class="spacer" />
+
+    <div class="divider" />
+
+    <div class="inline-block text-left">
+      <div class="mt-2">
+        <span class="node-nft__blue-text">Cost: </span>
+        {{ cost }} $POLAR
+      </div>
+      <div class="mt-1">
+        <span class="node-nft__blue-text">Claim tax: </span>
+        {{ claimTax }}%
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "nuxt-property-decorator";
+
+@Component({
+  props: {
+    name: { type: String },
+    dailyEarnings: { type: Number },
+    cost: { type: Number },
+    claimTax: { type: Number },
+  },
+})
+export default class NodeNft extends Vue {}
+</script>
+
+<style scoped>
+.spacer {
+  height: 127px;
+}
+
+.divider {
+  height: 1px;
+  flex-grow: 0;
+  background-color: rgba(255, 255, 255, 0.1);
+}
+
+.node-nft {
+  min-width: 180px;
+  cursor: pointer;
+  text-align: center;
+  font-size: 12px;
+  height: 242px;
+  flex-grow: 1;
+  padding: 12px 0;
+  border-radius: 14px;
+  box-shadow: 0 0 4px 4px rgba(0, 198, 237, 0.5);
+  border: solid 1px #00c6ed;
+  background-color: #17171b;
+  font-family: WorkSans;
+  font-weight: 500;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  color: #fff;
+}
+
+.node-nft__title {
+  font-size: 16px;
+}
+
+.node-nft__blue-text {
+  color: #00c6ed;
+}
+</style>
--- pages/nodes.vue
+++ pages/nodes.vue
@@ -1,10 +1,6 @@
 <template>
-<div class="flex flex-col md:mx-[197px] mx-[10%] mt-[30px] md:mt-4">
+  <div class="flex flex-col md:mx-[197px] mx-[10%] mt-[30px] md:mt-4">
     <span class="text-[24px] text-white">Node Station 🚉️</span>
-    <div class="mt-[32px]">
-      <CreateNode :nodeList="nodeName"
-       :list="myNodeList"/>
-    </div>
     <div class="md:flex flex-wrap gap-2 md:gap-[24px] mt-[32px]">
       <DataTable
         v-for="(item, i) in nodeStation"
@@ -12,87 +8,97 @@
         :title="item.title"
         :icon="item.icon"
         :price="item.price"
-        :percentage="item.percentage"/>
+        :percentage="item.percentage"
+      />
     </div>
-    <div class="md:mt-[32px]">
-      <NodeTable :items="myNodeData"/>
+    <span class="mt-[64px] text-[24px] text-white">Create Node 🗻️</span>
+    <div class="md:flex flex-wrap gap-2 md:gap-[28px] mt-[32px]">
+      <NodeNft
+        v-for="(node, i) of nodeNfts"
+        :key="`${name}-${i}`"
+        :name="node.name"
+        :daily-earnings="node.dailyEarnings"
+        :cost="node.cost"
+        :claim-tax="node.claimTax"
+      />
     </div>
-</div>
+  </div>
 </template>
 
 <script lang="ts">
 import { mapGetters } from "vuex";
-import { Component, Prop, Vue } from 'nuxt-property-decorator'
-import AlertComponents from '~/components/AlertComponents.vue'
+import { Component, Prop, Vue } from "nuxt-property-decorator";
+import AlertComponents from "~/components/AlertComponents.vue";
 
-import axios from "axios"
+import axios from "axios";
 
-// import { abi as NODE } from "~/hardhat/artifacts/contracts/Token.sol/PolarNodes.json";
-// import { abi as DAI } from "~/hardhat/artifacts/contracts/dai.sol/DAI.json";
-import { abi as NODER } from "~/hardhat/artifacts/contracts/NODERewardManager.sol/NODERewardManager.json"
-import { abi as POLAR } from "~/hardhat/artifacts/contracts/PolarNodes.sol/PolarNodes.json"
-import Defalut from "~/layouts/default.vue"
+import { abi as NODER } from "~/hardhat/artifacts/contracts/NODERewardManager.sol/NODERewardManager.json";
+import { abi as POLAR } from "~/hardhat/artifacts/contracts/PolarNodes.sol/PolarNodes.json";
+import Defalut from "~/layouts/default.vue";
 
-import {WalletModule} from '~/store'
+import { WalletModule } from "~/store";
 
-const {
-  Token,
-  PolarToken
-} = require("~/hardhat/scripts/address.js");
+const { Token, PolarToken } = require("~/hardhat/scripts/address.js");
 
-declare var window: any
+declare var window: any;
 
-@Component ({
-  components: {AlertComponents},
+@Component({
+  components: { AlertComponents },
 })
 export default class Nodes extends Vue {
-
   public nodeStation = [
     {
-      icon: require('../assets/img/nodesIcon/totalnodes_icon.svg'),
-      title: "Pending Rewards",
+      icon: require("../assets/img/nodesIcon/totalnodes_icon.svg"),
+      title: "Total Nodes",
       price: "0",
-      percentage: "0"
+      percentage: "0",
     },
     {
-      icon: require('../assets/img/nodesIcon/mynodes_icon.svg'),
+      icon: require("../assets/img/nodesIcon/mynodes_icon.svg"),
       title: "My Nodes",
       price: "0",
-      percentage: "0"
+      percentage: "0",
     },
     {
-      icon: require('../assets/img/nodesIcon/polarbalance_icon.svg'),
+      icon: require("../assets/img/nodesIcon/polarbalance_icon.svg"),
       title: "My $POLAR Balance",
       price: "0",
-      percentage: "0"
+      percentage: "0",
     },
-  ]
+  ];
 
-  private nodeData =[
+  public nodeNfts = [
+    { name: "Fuji", dailyEarnings: 0.45, cost: 30, claimTax: 1 },
+    { name: "Mont Blanc", dailyEarnings: 0.99, cost: 55, claimTax: 3 },
+    { name: "Killimanjaro", dailyEarnings: 5, cost: 250, claimTax: 5 },
+    { name: "Ushuaia", dailyEarnings: 9.2, cost: 400, claimTax: 8 },
+    { name: "Everest", dailyEarnings: 35, cost: 1000, claimTax: 10 },
+  ];
+
+  private nodeData = [
     {
-      nodeType : 'test1',
-      nodeCounter:3
+      nodeType: "test1",
+      nodeCounter: 3,
     },
     {
-      nodeType : 'test2',
-      nodeCounter:4
+      nodeType: "test2",
+      nodeCounter: 4,
     },
     {
-     nodeType : 'test3',
-     nodeCounter:5
-    }
-  ]
+      nodeType: "test3",
+      nodeCounter: 5,
+    },
+  ];
 
-  private nodeName : any = []
-  private nodeCounter : any = []
-  private myNodeData : any = []
-  private counterTemp : any = []
-  private myNodeList : any = []
-  private nodeNameList : any = []
-  private nodeInst : any
-  
+  private nodeName: any = [];
+  private nodeCounter: any = [];
+  private myNodeData: any = [];
+  private counterTemp: any = [];
+  private myNodeList: any = [];
+  private nodeNameList: any = [];
+  private nodeInst: any;
 
-  private getFromattedNb(nb : any) : string {
+  private getFromattedNb(nb: any): string {
     nb = nb.toLocaleString();
     if (nb.indexOf(".") == -1) return nb;
     else {
@@ -101,100 +107,104 @@ export default class Nodes extends Vue {
     }
     return nb.substr(0, nb.indexOf(".") + 3);
   }
-  private async created() : Promise<void> {
+  private async created(): Promise<void> {
     this.listenConnectEvent();
     this.intervalFetchData();
   }
 
-  public intervalFetchData() : void {
+  public intervalFetchData(): void {
     setInterval(() => {
       this.listenConnectEvent();
     }, 15000);
   }
-  private async listenConnectEvent() : Promise<void> {
-    this.myNodeData = []
-    
+  private async listenConnectEvent(): Promise<void> {
+    this.myNodeData = [];
+
     if (window.ethereum) {
       const ethers = require("ethers");
-      
+
       const provider = new ethers.providers.Web3Provider(
         window.ethereum,
         "any"
       );
-      try{
+      try {
         const signer = provider.getSigner();
-  
+
         const pnode = new ethers.Contract(Token, NODER, signer);
         const polar = new ethers.Contract(PolarToken, POLAR, signer);
 
-        let tmp      
-        if(WalletModule.walletaddress){
-          tmp = await pnode.calculateAllClaimableRewards(WalletModule.walletaddress);        
-          this.nodeStation[0].price = this.getFromattedNb(ethers.utils.formatEther(tmp._hex));
+        let tmp;
+        if (WalletModule.walletaddress) {
+          tmp = await pnode.calculateAllClaimableRewards(
+            WalletModule.walletaddress
+          );
+          this.nodeStation[0].price = this.getFromattedNb(
+            ethers.utils.formatEther(tmp._hex)
+          );
 
           tmp = await pnode.getTotalCreatedNodesOf(WalletModule.walletaddress);
-          this.nodeStation[1].price = parseInt(tmp._hex, 16).toString();        
+          this.nodeStation[1].price = parseInt(tmp._hex, 16).toString();
           tmp = await polar.balanceOf(WalletModule.walletaddress);
-          this.nodeStation[2].price = this.getFromattedNb(ethers.utils.formatEther(tmp._hex));
+          this.nodeStation[2].price = this.getFromattedNb(
+            ethers.utils.formatEther(tmp._hex)
+          );
         }
         tmp = await pnode.getNodeTypesSize();
-        let nodeSize = parseInt(tmp._hex, 16);      
-        
+        let nodeSize = parseInt(tmp._hex, 16);
+
         let tempNodeName = [];
-        for(let i = 0 ; i < nodeSize ; i++)
-        {        
-          tempNodeName.push(pnode.getNodeTypeNameAtIndex(i))
+        for (let i = 0; i < nodeSize; i++) {
+          tempNodeName.push(pnode.getNodeTypeNameAtIndex(i));
         }
-        await Promise.all(tempNodeName).then((res)=>{        
-          this.nodeName = []        
-          for(let i = 0 ; i < res.length ; i++)
-          {
+        await Promise.all(tempNodeName).then(res => {
+          this.nodeName = [];
+          for (let i = 0; i < res.length; i++) {
             this.nodeInst = {
-              nodeName : res[i] + " (Level " + (i+1) + ")", 
-              nodeValue : res[i].toString()
-            }
-            this.nodeName.push(this.nodeInst)          
+              nodeName: res[i] + " (Level " + (i + 1) + ")",
+              nodeValue: res[i].toString(),
+            };
+            this.nodeName.push(this.nodeInst);
           }
-        })
+        });
 
-        let tempCounter = []
+        let tempCounter = [];
 
-        for(let i = 0 ; i < this.nodeName.length ; i++)
-        {        
-          tempCounter.push(pnode.getNodeTypeOwnerNumber(this.nodeName[i].nodeValue,WalletModule.walletaddress))
+        for (let i = 0; i < this.nodeName.length; i++) {
+          tempCounter.push(
+            pnode.getNodeTypeOwnerNumber(
+              this.nodeName[i].nodeValue,
+              WalletModule.walletaddress
+            )
+          );
         }
 
-        await Promise.all(tempCounter).then((res : any)=>{                
-          for(let index in res)
-            this.nodeCounter[index] = parseInt(res[index]._hex, 16)
-        })
+        await Promise.all(tempCounter).then((res: any) => {
+          for (let index in res)
+            this.nodeCounter[index] = parseInt(res[index]._hex, 16);
+        });
 
-        this.myNodeList = []
+        this.myNodeList = [];
         let index = 0;
-        for(let i = 0 ; i < this.nodeName.length ; i++)
-        {
-          
-          if(this.nodeCounter[i]!=0)
-          {
-            this.counterTemp  = {
-              nodeIndex : i + 1,
+        for (let i = 0; i < this.nodeName.length; i++) {
+          if (this.nodeCounter[i] != 0) {
+            this.counterTemp = {
+              nodeIndex: i + 1,
               nodeType: this.nodeName[i].nodeValue,
-              nodeCounter: this.nodeCounter[i]
-            }
+              nodeCounter: this.nodeCounter[i],
+            };
             this.myNodeData.push(this.counterTemp);
-            for(let j = 0 ; j < this.nodeCounter[i] ; j++)
-            {
+            for (let j = 0; j < this.nodeCounter[i]; j++) {
               let temp = {
-                nodeName : this.nodeName[i].nodeValue,
-                index : index++, 
-              }
-              this.myNodeList.push(temp)
-            }   
-          }        
+                nodeName: this.nodeName[i].nodeValue,
+                index: index++,
+              };
+              this.myNodeList.push(temp);
+            }
+          }
         }
-      } catch(err) {
+      } catch (err) {
         (this.$root.$refs.alert as Defalut).AcceptMetamask();
-      }      
+      }
     }
   }
 }
