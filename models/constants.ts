cecff65d9277a24820e252f395d2b99db66f2624
salty <you@example.com>
2022-03-20 00:19:54 +0400

    images

--- models/constants.ts
+++ models/constants.ts
@@ -20,15 +20,15 @@ export type Url = "fuji" | "mont-blanc" | "kilimanjaro" | "ushuaia" | "everest";
 
 export const NODENAME_TO_IMAGE = {
   [NodeNftNames.Fuji]:
-    "https://ipfs.io/ipfs/QmPL9nCnvhH7bxPmnBPVbqoTUbYJJBp6wKuFgZUCQ6YE85?filename=FUJI%20FIXE.jpg",
+    require("../assets/PACK/FUJI-FIXE.jpg"),
   [NodeNftNames.MontBlanc]:
-    "https://ipfs.io/ipfs/QmY4qYu8jJQKzdNghzNsSdovVacyv9qEAeekHLPfwMCPRW?filename=MONT%20BLANC%20FIXE.jpg",
+  require("../assets/PACK/MONT BLANC FIXE.jpg"),
   [NodeNftNames.Kilimanjaro]:
-    "https://ipfs.io/ipfs/QmPC1raEZ4acMve2XDcRbY7yVLjwrrMYTS8xJTHhavJx32?filename=KILIMANDJARO%20FIXE.jpg",
+  require("../assets/PACK/KILIMANDJARO FIXE.jpg"),
   [NodeNftNames.Ushuaia]:
-    "https://ipfs.io/ipfs/QmdwE4ZbMzKUR1CHP2cbCPitnccChqiaEEGsyuMx4kn1np?filename=USHUAIA%20RENDER0133.jpg",
+  require("../assets/PACK/USHUAIA RENDER0133.jpg"),
   [NodeNftNames.Everest]:
-    "https://ipfs.io/ipfs/QmNkSYLUNGdiGfZjoC68C5qth9PKkZFkge9m5xtBhiH27L?filename=EVEREST%20FIXE.jpg",
+  require("../assets/PACK/EVEREST FIXE.jpg"),
 };
 
 export const NODENAME_TO_VIDEO = {
