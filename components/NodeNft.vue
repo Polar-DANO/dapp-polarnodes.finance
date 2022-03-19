933980a93f3295b582e688a440434fd09ef72a1e
Mehdi Tagaizine <mtagaizi@namek.io>
2022-03-19 13:48:31 +0100

    refactor: media queries

--- components/NodeNft.vue
+++ components/NodeNft.vue
@@ -93,13 +93,24 @@ export default class NodeNft extends Vue {
   box-shadow: 0 0 14px 14px rgba(0, 198, 237, 0.5);
 }
 
+@media-query min-width(1440px) {
+  .node-nft {
+    flex-grow: 0;
+  }
+}
+
+@media-query max-width(1439px) {
+  .node-nft {
+    flex-grow: 1;
+  }
+}
+
 .node-nft {
-  min-width: 180px;
+  min-width: 160px;
   cursor: pointer;
   text-align: center;
   font-size: 12px;
   height: 242px;
-  flex-grow: 1;
   padding: 12px 0;
   border-radius: 14px;
   border: solid 1px #00c6ed;
