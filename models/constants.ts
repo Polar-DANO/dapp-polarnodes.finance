da292a0eea7aab66ba1ddb4e284a2eb48cc6b7a1
Mehdi Tagaizine <mtagaizi@namek.io>
2022-03-19 01:15:05 +0100

    feat: reworked create node and added buybox page

---
+++ models/constants.ts
@@ -0,0 +1,15 @@
+export const luckyBoxes = [
+  { index: 1, name: "Lucky Box #1", cost: 30 },
+  { index: 2, name: "Lucky Box #2", cost: 55 },
+  { index: 3, name: "Lucky Box #3", cost: 250 },
+  { index: 4, name: "Lucky Box #4", cost: 400 },
+];
+
+export const LUCKY_BOX_BY_INDEX = {
+  [1]: { index: 1, name: "Lucky Box #1", cost: 30 },
+  [2]: { index: 2, name: "Lucky Box #2", cost: 55 },
+  [3]: { index: 3, name: "Lucky Box #3", cost: 250 },
+  [4]: { index: 4, name: "Lucky Box #4", cost: 400 },
+};
+
+export type LuckyBoxId = "1" | "2" | "3" | "4";
