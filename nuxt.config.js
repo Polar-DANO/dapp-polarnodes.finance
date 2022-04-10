6842e4f3013cc699a52fc33224d97d168ab169da
salty <you@example.com>
2022-04-11 00:03:50 +0400

    v3 live

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,7 @@ export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
   server: {
-    port: 7000, // default: 3000
+    port: 7500, // default: 3000
     host: '0.0.0.0', // default: localhost
   }, // other configs
   generate: {
