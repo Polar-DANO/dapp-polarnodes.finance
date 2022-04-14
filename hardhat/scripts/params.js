279763a19c160d45c8b233b54ec7781997594b42
salty <you@example.com>
2022-04-14 19:55:14 +0400

    params

--- hardhat/scripts/params.js
+++ hardhat/scripts/params.js
@@ -1,363 +1,481 @@
-const { ethers } = require('hardhat')
-const address = require('./address')
+const { ethers } = require("hardhat");
+const address = require("./address");
 
 // NodeTypes
 const NodeType0 = {
-  name: 'Fuji',
-  values: [
-    1000000, // maxCount
-    ethers.utils.parseUnits('30', 18), // price
-    14400, // claimTime
-    ethers.utils.parseUnits('0.075', 18), // rewardAmount
-    4000, // claimTaxRoi
-    0, // maxLevelUpUser
-    0, // maxLevelUpTotal
-    20, // maxCreationPendingUser
-    1000000, // maxCreationPendingTotal
-    1000000, // maxUser
-    500, // isBoostedNftRate
-    1000, // isBoostedNftProbability
-    7 * 24 * 3600, // obtainingTimeReference
-    100, // obtainingTimeRate
-    500, // isBoostedTokenRate
-    1 * 24 * 3600, // noClaimTimeReference
-    ethers.utils.parseUnits('1', 18), // noClaimRewardAmount
-    0, // global tax
-    7 * 24 * 3600, // claimTimeReference
-    100, // claimTimeRate
-    10, // maxMultiObtaining
-    0 // maxMultiClaim
-  ]
+	name: "Fuji",
+	values: [
+		10000000, // maxCount
+		ethers.utils.parseUnits("45", 18), // price
+		14400, // claimTime
+		ethers.utils.parseUnits("0.075", 18), // rewardAmount
+		1000, // claimTaxRoi
+		0, // maxLevelUpUser
+		0, // maxLevelUpTotal
+		5, // maxCreationPendingUser
+		10000000, // maxCreationPendingTotal
+		10000000, // maxUser
+		500, // isBoostedNftRate
+		1000, // isBoostedNftProbability
+		7 * 24 * 3600, // obtainingTimeReference
+		100, // obtainingTimeRate
+		0, // isBoostedTokenRate
+		14 * 24 * 3600, // noClaimTimeReference
+		ethers.utils.parseUnits("1", 18), // noClaimRewardAmount
+		200, // global tax
+		12 * 24 * 3600, // claimTimeReference
+		2000, // claimTimeRate
+		0, // maxMultiObtaining
+		5, // maxMultiClaim
+	]
 }
 const NodeType1 = {
-  name: 'Mont Blanc',
-  values: [
-    50000, // maxCount
-    ethers.utils.parseUnits('55', 18), // price
-    14400, // claimTime
-    ethers.utils.parseUnits('0.165', 18), // rewardAmount
-    4000, // claimTaxRoi
-    10, // maxLevelUpUser
-    10000, // maxLevelUpTotal
-    5, // maxCreationPendingUser
-    10000, // maxCreationPendingTotal
-    50000, // maxUser
-    500, // isBoostedNftRate
-    1000, // isBoostedNftProbability
-    7 * 24 * 3600, // obtainingTimeReference
-    100, // obtainingTimeRate
-    500, // isBoostedTokenRate
-    1 * 24 * 3600, // noClaimTimeReference
-    ethers.utils.parseUnits('1', 18), // noClaimRewardAmount
-    0, // global tax
-    7 * 24 * 3600, // claimTimeReference
-    100, // claimTimeRate
-    10, // maxMultiObtaining
-    0 // maxMultiClaim
-  ]
+	name: "Mont Blanc",
+	values: [
+		10000000, // maxCount
+		ethers.utils.parseUnits("80", 18), // price
+		14400, // claimTime
+		ethers.utils.parseUnits("0.165", 18), // rewardAmount
+		1500, // claimTaxRoi
+		10, // maxLevelUpUser
+		10000000, // maxLevelUpTotal
+		5, // maxCreationPendingUser
+		10000000, // maxCreationPendingTotal
+		10000000, // maxUser
+		500, // isBoostedNftRate
+		1000, // isBoostedNftProbability
+		7 * 24 * 3600, // obtainingTimeReference
+		100, // obtainingTimeRate
+		0, // isBoostedTokenRate
+		14 * 24 * 3600, // noClaimTimeReference
+		ethers.utils.parseUnits("2", 18), // noClaimRewardAmount
+		300, // global tax
+		15 * 24 * 3600, // claimTimeReference
+		2000, // claimTimeRate
+		0, // maxMultiObtaining
+		5, // maxMultiClaim
+	]
 }
 const NodeType2 = {
-  name: 'Kilimanjaro',
-  values: [
-    2000, // maxCount
-    ethers.utils.parseUnits('250', 18), // price
-    14400, // claimTime
-    ethers.utils.parseUnits('0.8333333333', 18), // rewardAmount
-    4000, // claimTaxRoi
-    2, // maxLevelUpUser
-    100, // maxLevelUpTotal
-    1, // maxCreationPendingUser
-    100, // maxCreationPendingTotal
-    100, // maxUser
-    500, // isBoostedNftRate
-    1000, // isBoostedNftProbability
-    7 * 24 * 3600, // obtainingTimeReference
-    100, // obtainingTimeRate
-    500, // isBoostedTokenRate
-    1 * 24 * 3600, // noClaimTimeReference
-    ethers.utils.parseUnits('1', 18), // noClaimRewardAmount
-    0, // global tax
-    7 * 24 * 3600, // claimTimeReference
-    100, // claimTimeRate
-    10, // maxMultiObtaining
-    0 // maxMultiClaim
-  ]
+	name: "Kilimanjaro",
+	values: [
+		10000, // maxCount
+		ethers.utils.parseUnits("315", 18), // price
+		14400, // claimTime
+		ethers.utils.parseUnits("0.8333333333", 18), // rewardAmount
+		1700, // claimTaxRoi
+		0, // maxLevelUpUser
+		500, // maxLevelUpTotal
+		5, // maxCreationPendingUser
+		500, // maxCreationPendingTotal
+		2000, // maxUser
+		500, // isBoostedNftRate
+		1000, // isBoostedNftProbability
+		7 * 24 * 3600, // obtainingTimeReference
+		100, // obtainingTimeRate
+		0, // isBoostedTokenRate
+		14 * 24 * 3600, // noClaimTimeReference
+		ethers.utils.parseUnits("10", 18), // noClaimRewardAmount
+		500, // global tax
+		18 * 24 * 3600, // claimTimeReference
+		2000, // claimTimeRate
+		0, // maxMultiObtaining
+		5, // maxMultiClaim
+	]
 }
 const NodeType3 = {
-  name: 'Ushuaia',
-  values: [
-    1000, // maxCount
-    ethers.utils.parseUnits('400', 18), // price
-    14400, // claimTime
-    ethers.utils.parseUnits('1.5333333333', 18), // rewardAmount
-    4000, // claimTaxRoi
-    2, // maxLevelUpUser
-    0, // maxLevelUpTotal
-    1, // maxCreationPendingUser
-    0, // maxCreationPendingTotal
-    50, // maxUser
-    500, // isBoostedNftRate
-    1000, // isBoostedNftProbability
-    7 * 24 * 3600, // obtainingTimeReference
-    100, // obtainingTimeRate
-    500, // isBoostedTokenRate
-    1 * 24 * 3600, // noClaimTimeReference
-    ethers.utils.parseUnits('1', 18), // noClaimRewardAmount
-    0, // global tax
-    7 * 24 * 3600, // claimTimeReference
-    100, // claimTimeRate
-    10, // maxMultiObtaining
-    0 // maxMultiClaim
-  ]
+	name: "Ushuaia",
+	values: [
+		10000, // maxCount
+		ethers.utils.parseUnits("460", 18), // price
+		14400, // claimTime
+		ethers.utils.parseUnits("1.5333333333", 18), // rewardAmount
+		2000, // claimTaxRoi
+		0, // maxLevelUpUser
+		500, // maxLevelUpTotal
+		2, // maxCreationPendingUser
+		500, // maxCreationPendingTotal
+		2000, // maxUser
+		500, // isBoostedNftRate
+		1000, // isBoostedNftProbability
+		7 * 24 * 3600, // obtainingTimeReference
+		100, // obtainingTimeRate
+		0, // isBoostedTokenRate
+		14 * 24 * 3600, // noClaimTimeReference
+		ethers.utils.parseUnits("16", 18), // noClaimRewardAmount
+		700, // global tax
+		21 * 24 * 3600, // claimTimeReference
+		2000, // claimTimeRate
+		0, // maxMultiObtaining
+		5, // maxMultiClaim
+	]
 }
 const NodeType4 = {
-  name: 'Everest',
-  values: [
-    1000, // maxCount
-    ethers.utils.parseUnits('1000', 18), // price
-    14400, // claimTime
-    ethers.utils.parseUnits('5.8333333333', 18), // rewardAmount
-    4000, // claimTaxRoi
-    2, // maxLevelUpUser
-    2, // maxLevelUpTotal
-    1, // maxCreationPendingUser
-    2, // maxCreationPendingTotal
-    50, // maxUser
-    500, // isBoostedNftRate
-    1000, // isBoostedNftProbability
-    7 * 24 * 3600, // obtainingTimeReference
-    100, // obtainingTimeRate
-    500, // isBoostedTokenRate
-    1 * 24 * 3600, // noClaimTimeReference
-    ethers.utils.parseUnits('1', 18), // noClaimRewardAmount
-    0, // global tax
-    7 * 24 * 3600, // claimTimeReference
-    100, // claimTimeRate
-    10, // maxMultiObtaining
-    0 // maxMultiClaim
-  ]
+	name: "Everest",
+	values: [
+		7500, // maxCount
+		ethers.utils.parseUnits("1200", 18), // price
+		14400, // claimTime
+		ethers.utils.parseUnits("5.8333333333", 18), // rewardAmount
+		2200, // claimTaxRoi
+		0, // maxLevelUpUser
+		500, // maxLevelUpTotal
+		1, // maxCreationPendingUser
+		500, // maxCreationPendingTotal
+		3000, // maxUser
+		500, // isBoostedNftRate
+		1000, // isBoostedNftProbability
+		7 * 24 * 3600, // obtainingTimeReference
+		100, // obtainingTimeRate
+		0, // isBoostedTokenRate
+		14 * 24 * 3600, // noClaimTimeReference
+		ethers.utils.parseUnits("60", 18), // noClaimRewardAmount
+		900, // global tax
+		24 * 24 * 3600, // claimTimeReference
+		2000, // claimTimeRate
+		0, // maxMultiObtaining
+		5, // maxMultiClaim
+	]
 }
 
 // PolarNode
 const PolarNode = {
-  baseUri: 'https://PolarNode.com/'
+	baseUri: "https://api.polar.financial/node/"
 }
 
 // PolarLuckyBox
 const PolarLuckyBox = {
-  baseUri: 'https://PolarLuckyBox.com/'
+	baseUri: "https://api.polar.financial/luckybox/"
 }
 
 // Swapper
 const Swapper = {
-  payees: address.Payees,
-  shares: [25, 25, 21, 21, 8],
-  addresses: [
-    address.Token,
-    address.Futur,
-    address.Distri,
-    address.LpHandler,
-    address.Router,
-    address.Native,
-    address.Pair
-  ],
-  fees: [1500, 7000, 500],
-  swapTokensAmounts: [
-    ethers.utils.parseUnits('30', 18),
-    ethers.utils.parseUnits('0.001', 18)
-  ]
+	payees: address.Payees,
+	shares: [25, 21, 21, 21, 12],
+	addresses: [
+		address.Token,
+		address.Futur,
+		address.Distri,
+		address.LpHandler,
+		address.Router,
+		address.Native,
+		address.Pair
+	],
+	fees: [500, 6500, 500], // [futur, rewards, lp]
+	swapTokensAmounts: ethers.utils.parseUnits("30", 18),
 }
 
 // LuckyBoxes
 const LuckyBox0 = {
-  name: 'Lucky Box 1',
-  priceTokens: ethers.utils.parseUnits('120', 18),
-  probability: [
-    3300, 5000, 1500, 195, 5
-  ],
-  nodeType: [
-    'Fuji', 'Mont Blanc', 'Kilimanjaro', 'Ushuaia', 'Everest'
-  ],
-  feature: [
-    '', '', '', '', ''
-  ],
-  remaining: [
-    10000, 10000, 10000, 10000, 10000
-  ],
-  maxBox: 10000,
-  maxUser: 500,
-  attribute: ''
+	name: "Neutral Lucky Box",
+	priceTokens: ethers.utils.parseUnits("135", 18),
+	probability: [
+		8000, 1300, 200, 180, 170, 70, 65, 10, 5
+	],
+	nodeType: [
+		"Mont Blanc", "Kilimanjaro", "Ushuaia", "Fuji", "Fuji", "Mont Blanc", "Mont Blanc", "Kilimanjaro", "Kilimanjaro"
+	],
+	feature: [
+		"", "", "", "Diamond", "Emerald", "Diamond", "Emerald", "Diamond", "Emerald"
+	],
+	remaining: [
+		100000, 100000, 100000, 100000, 100000, 100000, 100000, 100000, 100000
+	],
+	maxBox: 100000,
+	maxUser: 100000,
+	attribute: ""
 }
 const LuckyBox1 = {
-  name: 'Lucky Box 2',
-  priceTokens: ethers.utils.parseUnits('300', 18),
-  probability: [
-    200, 200, 8000, 1500, 100
-  ],
-  nodeType: [
-    'Fuji', 'Mont Blanc', 'Kilimanjaro', 'Ushuaia', 'Everest'
-  ],
-  feature: [
-    '', '', '', '', ''
-  ],
-  remaining: [
-    10000, 10000, 10000, 10000, 10000
-  ],
-  maxBox: 5000,
-  maxUser: 200,
-  attribute: ''
+	name: "Fresh Lucky Box",
+	priceTokens: ethers.utils.parseUnits("350", 18),
+	probability: [
+		8300, 1200, 300, 150, 20, 15, 10, 5
+	],
+	nodeType: [
+		"Kilimanjaro", "Ushuaia", "Mont Blanc", "Mont Blanc", "Kilimanjaro", "Kilimanjaro", "Ushuaia", "Ushuaia"
+	],
+	feature: [
+		"", "", "Diamond", "Emerald", "Diamond", "Emerald", "Diamond", "Emerald"
+	],
+	remaining: [
+		100000, 100000, 100000, 100000, 100000, 100000, 100000, 100000
+	],
+	maxBox: 100000,
+	maxUser: 100000,
+	attribute: ""
 }
 const LuckyBox2 = {
-  name: 'Lucky Box 3',
-  priceTokens: ethers.utils.parseUnits('700', 18),
-  probability: [
-    1500, 6500, 2000
-  ],
-  nodeType: [
-    'Kilimanjaro', 'Ushuaia', 'Everest'
-  ],
-  feature: [
-    '', '', ''
-  ],
-  remaining: [
-    10000, 1000, 100
-  ],
-  maxBox: 1000,
-  maxUser: 100,
-  attribute: ''
+	name: "Icy Lucky Box",
+	priceTokens: ethers.utils.parseUnits("500", 18),
+	probability: [
+		8500, 800, 400, 200, 50, 35, 10, 5
+	],
+	nodeType: [
+		"Ushuaia", "Everest", "Kilimanjaro", "Kilimanjaro", "Ushuaia", "Ushuaia", "Everest", "Everest"
+	],
+	feature: [
+		"", "", "Diamond", "Emerald", "Diamond", "Emerald", "Diamond", "Emerald"
+	],
+	remaining: [
+		100000, 100000, 100000, 100000, 100000, 100000, 100000, 100000
+	],
+	maxBox: 100000,
+	maxUser: 100000,
+	attribute: ""
 }
 const LuckyBox3 = {
-  name: 'Lucky Box 4',
-  priceTokens: ethers.utils.parseUnits('1500', 18),
-  probability: [
-    4000, 2000, 2000, 2000
-  ],
-  nodeType: [
-    'Everest', 'Everest', 'Everest', 'Everest'
-  ],
-  feature: [
-    '', 'Gold', 'Diamond', 'Emerald'
-  ],
-  remaining: [
-    10000, 30, 20, 10
-  ],
-  maxBox: 500,
-  maxUser: 10,
-  attribute: ''
+	name: "Everest Lucky Box",
+	priceTokens: ethers.utils.parseUnits("1500", 18),
+	probability: [
+		8000, 1500, 500
+	],
+	nodeType: [
+		"Everest", "Everest", "Everest"
+	],
+	feature: [
+		"", "Diamond", "Emerald"
+	],
+	remaining: [
+		100000, 100000, 100000
+	],
+	maxBox: 100000,
+	maxUser: 100000,
+	attribute: ""
 }
-const NodeType4Feature0 = {
-  name: 'Gold',
-  rate: 500
-}
-const NodeType4Feature1 = {
-  name: 'Diamond',
-  rate: 1000
-}
-const NodeType4Feature2 = {
-  name: 'Emerald',
-  rate: 2000
-}
+// features for all
+const NodeType0Features = [
+	{
+		name: "Silver",
+		rate: 5500,
+	},
+	{
+		name: "Gold",
+		rate: 9500,
+	},
+	{
+		name: "Diamond",
+		rate: 17000,
+	},
+	{
+		name: "Emerald",
+		rate: 21000,
+	},
+]
+const NodeType1Features = [
+	{
+		name: "Silver",
+		rate: 3200,
+	},
+	{
+		name: "Gold",
+		rate: 6500,
+	},
+	{
+		name: "Diamond",
+		rate: 10000,
+	},
+	{
+		name: "Emerald",
+		rate: 15000,
+	},
+]
+const NodeType2Features = [
+	{
+		name: "Silver",
+		rate: 1760,
+	},
+	{
+		name: "Gold",
+		rate: 4000,
+	},
+	{
+		name: "Diamond",
+		rate: 8900,
+	},
+	{
+		name: "Emerald",
+		rate: 13500,
+	},
+]
+const NodeType3Features = [
+	{
+		name: "Silver",
+		rate: 1600,
+	},
+	{
+		name: "Gold",
+		rate: 3300,
+	},
+	{
+		name: "Diamond",
+		rate: 6600,
+	},
+	{
+		name: "Emerald",
+		rate: 10000,
+	},
+]
+const NodeType4Features = [
+	{
+		name: "Silver",
+		rate: 700,
+	},
+	{
+		name: "Gold",
+		rate: 1400,
+	},
+	{
+		name: "Diamond",
+		rate: 2800,
+	},
+	{
+		name: "Emerald",
+		rate: 5000,
+	},
+]
 
 const SwapperPath0 = {
-  token: address.Native,
-  pathIn: [
-    address.Native, address.Token
-  ],
-  pathOut: [
-    address.Native
-  ]
+	token: address.Native,
+	pathIn: [
+		address.Native, address.Token
+	],
+	pathOut: [
+		address.Null
+	]
 }
 const SwapperPath1 = {
-  token: address.Dai,
-  pathIn: [
-    address.Dai, address.Native, address.Token
-  ],
-  pathOut: [
-    address.Dai
-  ]
+	token: address.Dai,
+	pathIn: [
+		address.Dai, address.Native, address.Token
+	],
+	pathOut: [
+		address.Null
+	]
+}
+const SwapperPath2 = {
+	token: address.Thor,
+	pathIn: [
+		address.Thor, address.Native, address.Token
+	],
+	pathOut: [
+		address.Null
+	]
+}
+const SwapperPath3 = {
+	token: address.Vpnd,
+	pathIn: [
+		address.Vpnd, address.Native, address.Token
+	],
+	pathOut: [
+		address.Null
+	]
+}
+const SwapperPath4 = {
+	token: address.Fire,
+	pathIn: [
+		address.Fire, address.Native, address.Token
+	],
+	pathOut: [
+		address.Null
+	]
+}
+const SwapperPath5 = {
+	token: address.Mead,
+	pathIn: [
+		address.Mead, address.Usdc, address.Native, address.Token
+	],
+	pathOut: [
+		address.Null
+	]
+}
+const SwapperPath6 = {
+	token: address.Pxt2,
+	pathIn: [
+		address.Pxt2, address.Native, address.Token
+	],
+	pathOut: [
+		address.Null
+	]
+}
+const SwapperPath7 = {
+	token: address.Time,
+	pathIn: [
+		address.Time, address.Native, address.Token
+	],
+	pathOut: [
+		address.Null
+	]
 }
 
 const AddOwner = [
-  '0xdd2fd4581271e230360230f9337d5c0430bf44c0', // hardhat18 = distri
-  true // change super owner
+	"0xE964346CeA0fA249c3FE5EECb4FEA289964799Fa", // owner polar
+	true // change super owner // put true
 ]
 
 const NodeTypes = [
-  NodeType0, NodeType1, NodeType2, NodeType3, NodeType4
+	NodeType0, NodeType1, NodeType2, NodeType3, NodeType4
 ]
 
 const MarketPlace = {
-  fee: 1000,
-  auctionBeforeRef: 60,
-  auctionBeforeAdd: 60
+	fee: 200,
+	auctionBeforeRef: 60,
+	auctionBeforeAdd: 60
 }
 
 const MinPricesNode = {
-  names: [
-    NodeTypes[0].name,
-    NodeTypes[1].name,
-    NodeTypes[2].name,
-    NodeTypes[3].name,
-    NodeTypes[4].name
-  ],
-  offerPrices: [
-    ethers.utils.parseUnits('1', 18), // fuji
-    ethers.utils.parseUnits('1', 18), // mont blanc
-    ethers.utils.parseUnits('1', 18), // kilimanjaro
-    ethers.utils.parseUnits('1', 18), // ushuaia
-    ethers.utils.parseUnits('1', 18) // everest
-  ],
-  auctionPrices: [
-    ethers.utils.parseUnits('1', 18),
-    ethers.utils.parseUnits('1', 18),
-    ethers.utils.parseUnits('1', 18),
-    ethers.utils.parseUnits('1', 18),
-    ethers.utils.parseUnits('1', 18)
-  ]
+	names: [
+		NodeTypes[0].name,
+		NodeTypes[1].name,
+		NodeTypes[2].name,
+		NodeTypes[3].name,
+		NodeTypes[4].name,
+	],
+	offerPrices: [
+		ethers.utils.parseUnits("45", 18), // fuji
+		ethers.utils.parseUnits("80", 18), // mont blanc
+		ethers.utils.parseUnits("315", 18), // kilimanjaro
+		ethers.utils.parseUnits("460", 18), // ushuaia
+		ethers.utils.parseUnits("1200", 18), // everest
+	],
+	auctionPrices: [
+		ethers.utils.parseUnits("45", 18),
+		ethers.utils.parseUnits("80", 18),
+		ethers.utils.parseUnits("315", 18),
+		ethers.utils.parseUnits("460", 18),
+		ethers.utils.parseUnits("1200", 18),
+	],
 }
 
 const MinPricesLucky = {
-  names: [
-    LuckyBox0.name,
-    LuckyBox1.name,
-    LuckyBox2.name,
-    LuckyBox3.name
-  ],
-  offerPrices: [
-    ethers.utils.parseUnits('1', 18), // box 1
-    ethers.utils.parseUnits('1', 18), // box 2
-    ethers.utils.parseUnits('1', 18), // box 3
-    ethers.utils.parseUnits('1', 18) // box 4
-  ],
-  auctionPrices: [
-    ethers.utils.parseUnits('1', 18),
-    ethers.utils.parseUnits('1', 18),
-    ethers.utils.parseUnits('1', 18),
-    ethers.utils.parseUnits('1', 18)
-  ]
+	names: [
+		LuckyBox0.name,
+		LuckyBox1.name,
+		LuckyBox2.name,
+		LuckyBox3.name,
+	],
+	offerPrices: [
+		ethers.utils.parseUnits("135", 18), // box 1
+		ethers.utils.parseUnits("350", 18), // box 2
+		ethers.utils.parseUnits("500", 18), // box 3
+		ethers.utils.parseUnits("1500", 18), // box 4
+	],
+	auctionPrices: [
+		ethers.utils.parseUnits("135", 18),
+		ethers.utils.parseUnits("350", 18),
+		ethers.utils.parseUnits("500", 18),
+		ethers.utils.parseUnits("1500", 18),
+	],
 }
 
 module.exports = {
-  NodeType0,
-  NodeType1,
-  NodeType2,
-  NodeType3,
-  NodeType4,
-  PolarNode,
-  PolarLuckyBox,
-  Swapper,
-  LuckyBox0,
-  LuckyBox1,
-  LuckyBox2,
-  LuckyBox3,
-  NodeType4Feature0,
-  NodeType4Feature1,
-  NodeType4Feature2,
-  SwapperPath0,
-  SwapperPath1,
-  AddOwner,
-  MarketPlace,
-  NodeTypes,
-  MinPricesNode,
-  MinPricesLucky
+	NodeType0, NodeType1, NodeType2, NodeType3, NodeType4,
+	PolarNode, PolarLuckyBox, Swapper,
+	LuckyBox0, LuckyBox1, LuckyBox2, LuckyBox3,
+	NodeType0Features, NodeType1Features, NodeType2Features, 
+	NodeType3Features, NodeType4Features,
+	SwapperPath0, SwapperPath1, SwapperPath2, SwapperPath3,
+	SwapperPath4, SwapperPath5, SwapperPath6, SwapperPath7, 
+	AddOwner, MarketPlace,
+	NodeTypes, MinPricesNode, MinPricesLucky
 }
+
